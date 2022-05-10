778481d515b616d9bfbc47bc187ed3498ac548f6
AngelGM <cesargasca083@gmail.com>
2022-05-10 09:28:06 -0500

    Código JS para aparecer los elementos

---
+++ js/app.js
@@ -0,0 +1,11 @@
+const bloque  = document.querySelectorAll('.bloque')
+const h2      = document.querySelectorAll('.h2')
+
+h2.forEach(( eachH1, index) => {
+  h2[index].addEventListener('click', () => {
+    bloque.forEach((eachBloque, i) => {
+      bloque[i].classList.remove('activo');
+    })
+    bloque[index].classList.add('activo')
+  })
+})
