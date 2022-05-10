b4c76a568c2d983f1c4294154a399b28c24fa286
AngelGM <cesargasca083@gmail.com>
2022-05-10 10:01:08 -0500

    Ahora se puede deseleccionar un elemento activo

--- js/app.js
+++ js/app.js
@@ -3,9 +3,16 @@ const h2      = document.querySelectorAll('.h2')
 
 h2.forEach(( eachH1, index) => {
   h2[index].addEventListener('click', () => {
-    bloque.forEach((eachBloque, i) => {
-      bloque[i].classList.remove('activo');
-    })
-    bloque[index].classList.add('activo')
+    if (!bloque[index].classList.contains('activo')) {
+      bloque.forEach((eachBloque, i) => {
+        bloque[i].classList.remove('activo');
+      })
+      bloque[index].classList.add('activo')
+      
+    } else {
+      bloque.forEach((eachBloque, i) => {
+        bloque[i].classList.remove('activo');
+      })
+    }
   })
 })
